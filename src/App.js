93907c5b7d476c4e15eecc22bfa0002d345b09f6
mlunceford <mark7lunceford@gmail.com>
2019-10-28 10:44:49 -0600

    done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
+import {HashRouter} from 'react-router-dom'
+import routes from './routes'
+import {Link} from 'react-router-dom'
+
+
+
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <nav className='nav'>
-          <div>WestSide University</div> 
-          <div className='link-wrap'>
-              <div className='links'>Home</div>
-              <div className='links'>About</div> 
-          </div>
-        </nav>
-      </div>
+        <HashRouter>
+            <div>
+                <nav className='nav'>
+                    <div>WestSide University</div> 
+                    <div className='link-wrap'>
+                        <Link to='/'>Home</Link>
+                        <Link to='/About'>About</Link> 
+                    </div>
+                </nav>
+                {routes}
+            </div>
+        </HashRouter>
     )
   }
 }
